4420986a9998bf3a58d19a954bc9d761e62e9c84
amzadhossainjacky <amzadhossainjacky@gmail.com>
2024-04-10 02:43:16 +0600

    vue component created and register learn

---
+++ ch1/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h2>Local Register component</h2>
+    <HelloWorld />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ ch1/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const msg = "Hello World!"
+</script>
+
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<style scope>
+
+    h1{
+        font-size: 30px;
+        color: red;
+    }
+</style>
