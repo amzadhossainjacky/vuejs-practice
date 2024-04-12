8ad376cb91f61c8f64f0931977ec606874c16793
amzadhossainjacky <amzadhossainjacky@gmail.com>
2024-04-12 09:22:18 +0600

    text interpolation practice

---
+++ ch2/src/App.vue
@@ -0,0 +1,19 @@
+
+<!-- text interpolation -->
+<script setup>
+
+const num = 20;
+function disp() {
+  return "Print display";
+}
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <h1>{{ 20 + 2 }}</h1>
+  <h1>{{ num }}</h1>
+  <h1>{{ num +1 }}</h1>
+  <h1>{{ disp() }}</h1>
+</template>
+
+  
