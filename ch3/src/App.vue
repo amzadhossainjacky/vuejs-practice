710849ed5b72385023d3e2ea7179b74ea827d09e
amzadhossainjacky <amzadhossainjacky@gmail.com>
2024-04-14 14:48:40 +0600

    attribute binding learning

---
+++ ch3/src/App.vue
@@ -0,0 +1,8 @@
+<!-- text interpolation -->
+<script setup>
+  import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <HelloWorld />
+</template>
---
+++ ch3/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+
+<script setup>
+const attribute_id = "jack";
+const url = "https://youtube.com";
+const target = "blank";
+
+//dynamic object binding
+const dynamic_atrribute = {
+    href: "https://youtube.com",
+    target: "blank",
+}
+
+//dymanic attribute
+const dynamicArgumentName = "href";
+
+//boolean attribute
+const isDisabled = true;
+
+//dynamic argument
+</script>
+
+<template>
+    <div>
+        <h1>{{ attribute_id }}</h1>
+        <h2 id="jack">Id</h2>
+        <h3 v-bind:id="attribute_id">Id</h3>
+        <h4 :id="attribute_id">Id</h4>
+        <a :href="url" :target="target">Visit 1</a>
+
+        <!-- dynamic binding object -->
+        <a v-bind="dynamic_atrribute"> dynamic_atribute</a>
+        <br>
+
+        <!-- dynamic argument -->
+        <a :[dynamicArgumentName]="url" :target="target"> DynamicArgument</a>
+        <br>
+        
+        <!-- boolean attribute -->
+        <a :disabled="isDisabled" :href="url" :target="target"> visit(boolean attribute) </a>
+    </div>
+</template>
