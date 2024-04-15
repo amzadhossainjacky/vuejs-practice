c9fa01605f8939c0598bec67c96bcbb5445b3a8a
amzadhossainjacky <amzadhossainjacky@gmail.com>
2024-04-15 09:39:15 +0600

    vue reactive learning continue ..

---
+++ ch4/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<script setup>
+    import { reactive } from 'vue';
+
+    // without reactivity
+    /* let reactiveNum = {
+        a:10,
+        b:20
+    };
+
+    setTimeout(()=>{
+        console.log("SetTimeout")
+        console.log("Num:",reactiveNum)
+        reactiveNum.a = 30
+        console.log("a:",reactiveNum.a)
+        console.log("Num Changes:",reactiveNum)
+    }, 2000) */
+
+    // with reactivity
+    let reactiveNum = reactive({
+        a:10,
+        b:20
+    });
+
+    setTimeout(()=>{
+        console.log("SetTimeout")
+        console.log("Num:",reactiveNum)
+        reactiveNum.a = 30
+        console.log("a:",reactiveNum.a)
+        console.log("Num Changes:",reactiveNum)
+    }, 2000)
+    
+</script>
+
+<template>
+    <div> 
+        <h1> Sum:{{reactiveNum.a + reactiveNum.b}} </h1>
+    </div>
+</template>
