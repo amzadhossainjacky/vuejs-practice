d3e6a7fecb333a7a20408083abd70d09cee9ea57
amzadhossainjacky <amzadhossainjacky@gmail.com>
2024-04-20 16:52:08 +0600

    reactive learning

--- ch4/src/components/HelloWorld.vue
+++ ch4/src/components/HelloWorld.vue
@@ -16,10 +16,12 @@
     }, 2000) */
 
     // with reactivity
-    let reactiveNum = reactive({
+    /* let reactiveNum = reactive({
         a:10,
         b:20
     });
+
+
 
     setTimeout(()=>{
         console.log("SetTimeout")
@@ -27,12 +29,73 @@
         reactiveNum.a = 30
         console.log("a:",reactiveNum.a)
         console.log("Num Changes:",reactiveNum)
-    }, 2000)
+    }, 2000) */
+
+    //another example for without reactive
+    /* let num = {
+        count: 0
+    }
+    setTimeout(()=>{
+        console.log("count: ", num.count)
+        num.count = 40;
+        
+        console.log("SetTimeout")
+        console.log("count: ", num.count)
+
+    }, 3000); */
+
+    
+    // another example for without reactivity
+    /* let num = reactive({
+        count: 0
+    })
+    setTimeout(()=>{
+        console.log("count: ", num.count)
+        console.log(num)
+        num.count = 40;
+        
+        console.log("SetTimeout")
+        console.log("count: ", num.count)
+
+    }, 3000); */
+
+    //deeply reactive
+    let obj = reactive({
+       num: {
+        count: 0
+       },
+       users: [
+        'Jacky',
+        'Rainy'
+       ]
+    });
+
+    setTimeout(() => {
+        console.log(obj)
+        console.log("count: ", obj.num)
+        console.log("count: ", obj.num.count)
+        obj.num.count = 0;
+        
+        console.log("SetTimeout")
+        console.log("count: ", obj.num.count)
+
+        //array
+        console.log("array: ", obj.users)
+        obj.users.push('Shakib')
+        console.log("array: ", obj.users)
+    }, 3000);
     
 </script>
 
 <template>
     <div> 
-        <h1> Sum:{{reactiveNum.a + reactiveNum.b}} </h1>
+        <!-- <h1> Sum:{{reactiveNum.a + reactiveNum.b}} </h1> -->
+
+        <!-- another example -->
+        <!--  <h1> Count:{{num.count}} </h1> -->
+
+       <!-- deeply reactive -->
+        <h1> Count:{{obj.num.count}} </h1>
+        <h1> Count:{{obj.users}} </h1>
     </div>
 </template>
