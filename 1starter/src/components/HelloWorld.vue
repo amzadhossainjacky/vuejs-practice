cef45cd698091ec50e92f354e7ec1c4477d69743
amzadhossainjacky <amzadhossainjacky@gmail.com>
2024-04-20 20:56:11 +0600

    reactive applied in vue

---
+++ 1starter/src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<script setup>
+  let greeting = "Hello World"
+</script>
+
+<template>
+  <h1>{{ greeting }}</h1>
+</template>
---
+++ ch5/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<script setup>
+  import { ref } from 'vue';
+
+  //let count = ref()
+  // setTimeout(() => {
+  //   count.value = 30
+  // }, 3000);
+
+  // with ref
+  /* let result = {
+    count: ref(5)
+  }
+
+  setTimeout(() => {
+    console.log(result.count.value)
+  }, 300); */
+
+  //
+  let result = {
+    count: ref(5)
+  };
+
+  let { count } = result;
+
+  setTimeout(() => {
+    console.log(count)
+
+   // count.value = count.value + 3;
+   result.count.value = result.count.value + 3;
+    
+    console.log(count)
+  }, 3000);
+
+
+
+</script>
+
+<template>
+  <!-- <h1> Count:{{  count }}</h1> -->
+  <h1> Count:{{ count }}</h1>
+</template>
