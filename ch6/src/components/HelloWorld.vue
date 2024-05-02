ce557dc4e9765a54b12e948dd7a1ddac869fa824
amzadhossainjacky <amzadhossainjacky@gmail.com>
2024-05-02 10:00:07 +0600

    style class implementation

---
+++ ch6/src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { computed, ref } from "vue";
+
+//without ref
+/* let firstname = "Amzad Hossain";
+let lastname = "Jacky";
+
+function getName() {
+  return firstname + ' ' + lastname;
+}
+
+setTimeout(() => {
+  firstname = "Md. Amzad Hossain"
+}, 3000); */
+
+// with ref
+/* let firstname = ref("Amzad Hossain");
+let lastname = ref("Jacky") ;
+
+function getName() {
+  return firstname.value + ' ' + lastname.value;
+}
+
+setTimeout(() => {
+  firstname.value = "Md. Amzad Hossain"
+}, 3000);
+ */
+
+//computed property
+let firstname = ref("Amzad Hossain");
+let lastname = ref("Jacky") ;
+
+let fullName = computed(() => {
+  console.log("Getter");
+  return firstname.value + " " + lastname.value;
+});
+
+setTimeout(() => {
+  firstname.value = 'Md. Amzad Hossain'
+}, 3000);
+</script>
+
+<template>
+  <!-- <h1>Full Name: {{ getName() }}</h1> -->
+  <!-- with ref -->
+  <!-- <h1>Full Name: {{ getName() }}</h1> -->
+
+  <!-- computed property -->
+  <div>
+    <h2>Full Name: {{ fullName }}</h2>
+    <h2>Full Name: {{ fullName }}</h2>
+  </div>
+</template>
---
+++ ch7/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, reactive } from "vue";
+
+//ref implementation
+let cl = ref('white');
+let bg = ref('black');
+
+//reactive implementation
+let myStyle = reactive({
+  color: "blue",
+  backgroundColor: "yellow"
+})
+</script>
+
+<template>
+  <div>
+    <!-- v binding by style -->
+    <h1 v-bind:style="{ color: 'red', backgroundColor: 'green' }">
+      Hello World
+    </h1>
+    <h1 :style="{ color: 'green', backgroundColor: 'red' }">Hello World</h1>
+
+    <!-- v binding by style dynamic -->
+    <h1 :style="{ color: cl, backgroundColor: bg }">
+      Hello World
+    </h1>
+    <h1 :style="myStyle">Hello World</h1>
+  </div>
+</template>
