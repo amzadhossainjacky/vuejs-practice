e613d799fd5273a66b114827002564f84191798e
amzadhossainjacky <amzadhossainjacky@gmail.com>
2024-05-04 23:54:05 +0600

    list show example: v-for

---
+++ ch9/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, reactive, computed } from "vue";
+
+let employees = ref([
+  {
+    name: "Jacky",
+    salary: 25000,
+  },
+  {
+    name: "Mehedi",
+    salary: 30000,
+  },
+]);
+</script>
+
+<template>
+  <div
+    v-for="(employee, i) in employees"
+    :key="'employee_info_' + i"
+    v-bind:id="'employee_info_' + i"
+  >
+      <h1>Name: {{ employee.name }}</h1>
+      <h1>Salary: {{ employee.salary }}</h1>
+  </div>
+</template>
