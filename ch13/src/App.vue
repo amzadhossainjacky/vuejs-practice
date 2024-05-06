f742400a3b365ebf8e56ced80433d1db16b4f6dd
amzadhossainjacky <amzadhossainjacky@gmail.com>
2024-05-06 13:47:53 +0600

    component event updated

---
+++ ch13/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import HelloWorld from './components/HelloWorld.vue';
+  import { ref } from 'vue';
+  
+  let count = ref(0)
+
+  function increaseCounter(){
+    return count.value++ 
+  }
+
+</script>
+
+<template>
+  <h1>Count: {{ count }}</h1>
+  
+  <!-- <button @click="count++"> Increase</button> -->
+  <!-- <HelloWorld @increaseCounter="count++" /> -->
+  <!-- <HelloWorld @increaseCounter="increaseCounter" /> -->
+
+  <!-- increase by 11 -->
+  <HelloWorld @increaseCounter="(n)=>(count = count+n)" />
+</template>
---
+++ ch13/src/components/HelloWorld.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <div>
+    <!-- <button @click="$emit('increaseCounter')">Increase</button> -->
+    <button @click="$emit('increaseCounter', 11)">Increase</button>
+  </div>
+</template>
