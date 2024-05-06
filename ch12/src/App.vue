955581c7b5cb2c591b17d59a808663c61543043e
amzadhossainjacky <amzadhossainjacky@gmail.com>
2024-05-06 12:04:08 +0600

    vue props learning

---
+++ ch12/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import { ref, reactive, computed } from "vue";
+
+/* let fullName = ref("Md. Amzad Hossain"); */
+
+let firstName = ref("Naim")
+let lastName = ref("Jacky")
+
+
+setInterval(() => {
+  firstName.value = 'khalid'
+}, 3000);
+</script>
+
+<template>
+  <!-- static props -->
+  <!-- <HelloWorld  fullName="Md. Amzad Hossain Jacky"/> -->
+
+  <!-- dynamic props with cabab case -->
+  <!-- <HelloWorld :full-name="fullName" /> -->
+
+
+  <HelloWorld :firstName="firstName" :lastName="lastName" />
+
+
+
+</template>
---
+++ ch12/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, reactive, computed } from "vue";
+
+//static props
+/* let props = defineProps(['fullName']) */
+//dynamic props
+/* let props = defineProps(['fullName']) */
+
+//computed property
+
+let props = defineProps([
+  'firstName', 'lastName'
+])
+
+const data = computed(() => props.firstName)
+
+</script>
+
+<template>
+  <!-- static props -->
+  <!-- <h1>Static Props Name: {{ fullName }}</h1> -->
+
+  <!-- dynamic props -->
+  <!-- <h1>Dynamic Props Name: {{ fullName }}</h1> -->
+  <h1>Full Name: {{ data }} {{ props.lastName }}</h1>
+
+
+</template>
