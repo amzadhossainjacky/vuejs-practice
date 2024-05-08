eafb86cd8767ba3d498c5729a9f58a647a2796d9
amzadhossainjacky <amzadhossainjacky@gmail.com>
2024-05-08 13:20:53 +0600

    life cycle hook created

---
+++ ch16/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+  import HelloWorld from './components/HelloWorld.vue';
+  import { onBeforeMount,  onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted, ref} from 'vue'
+
+  const count = ref(0)
+  const showHideChildComponent = ref(true)
+
+  onBeforeMount(()=>{
+    console.log("onBeforeMount called - Parent")
+  })
+
+  onMounted(()=>{
+    console.log("onMounted called - Parent")
+  })
+
+  onBeforeUpdate(()=>{
+    console.log("onBeforeUpdate called - Parent")
+  })
+
+  onUpdated(()=>{
+    console.log("onUpdated called - Parent")
+  })
+
+  onBeforeUnmount(()=>{
+    console.log("onBeforeUnmount called - Parent")
+  })
+
+  onUnmounted(()=>{
+    console.log("onBeforeMount called - Parent")
+  })
+
+
+</script>
+
+<template>
+  <h1>Count: {{ count }}</h1>
+  <HelloWorld v-if="showHideChildComponent"/>
+  <button @click="showHideChildComponent = !showHideChildComponent">Hide/Show</button>
+  <button @click="count++">Increase Counter</button>
+</template>
---
+++ ch16/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<script setup>
+  let greeting = "Hello Bangladesh"
+  import { onBeforeMount,  onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted, ref} from 'vue'
+
+  
+  onBeforeMount(()=>{
+    console.log("onBeforeMount called - Child")
+  })
+
+  onMounted(()=>{
+    console.log("onMounted called - Child")
+  })
+
+  onBeforeUpdate(()=>{
+    console.log("onBeforeUpdate called - Child")
+  })
+
+  onUpdated(()=>{
+    console.log("onUpdated called - Child")
+  })
+
+  onBeforeUnmount(()=>{
+    console.log("onBeforeUnmount called - Child")
+  })
+
+  onUnmounted(()=>{
+    console.log("onBeforeMount called - Child")
+  })
+
+</script>
+
+<template>
+  <h2>{{ greeting }}</h2>
+</template>
