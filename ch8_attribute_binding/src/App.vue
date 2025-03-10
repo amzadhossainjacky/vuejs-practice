8c64c4796cf4708f33a5e723899a6d72bc9beedc
amzadhossainjacky <amzadhossainjacky@gmail.com>
2025-03-11 02:00:05 +0600

    vue attribute learned

---
+++ ch8_attribute_binding/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'; ''
+const myid = "myid1"
+const myid2 = "myid2"
+const url = "https://www.google.com/"
+const isDisabled = true
+</script>
+
+<template>
+  <div>
+    <h1 v-bind:id="myid">Attribute Binding </h1>
+    <h1 :id="myid2">Attribute Binding </h1>
+    <a v-bind:href="url">Google </a>
+    <br>
+    <button v-bind:disabled="isDisabled"> click</button>
+    <p>
+      <HelloWorld />
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
---
+++ ch8_attribute_binding/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup>
+const msg = "Hello Bangladesh"
+</script>
+
+<template>
+    <div>
+        <h1> {{ msg }} </h1>
+    </div>
+</template>
+
+<style scope>
+
+</style>
