c52e6435d1028d8b43fc2a03b970c774123f812c
amzadhossainjacky <amzadhossainjacky@gmail.com>
2025-03-25 23:55:29 +0600

    style binding

---
+++ ch15_style_binding/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'; 
+import { reactive } from 'vue';
+
+/* reactive object */
+
+const styleObj = reactive({
+  color: 'red',
+  backgroundColor: 'yellow'
+})
+const myStyleObj = reactive({
+  fontSize: "50px" 
+})
+
+</script>
+
+<template>
+  <div>
+    <h1 style="color: red; background: black;">Style 1</h1>
+    <h1 :style="{ color: 'white', backgroundColor: 'green'}">Style Binding 2</h1>
+    <h1 :style="styleObj">Style Binding 3</h1>
+    <h1 :style="[styleObj, myStyleObj]">Style Binding 4</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
