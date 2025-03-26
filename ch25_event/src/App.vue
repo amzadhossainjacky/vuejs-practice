56d343997c3f0f4b473eeff1be05e7fd5acbe492
amzadhossainjacky <amzadhossainjacky@gmail.com>
2025-03-26 17:53:25 +0600

    event handaler completed

---
+++ ch25_event/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'; 
+import { ref } from 'vue'
+
+let count = ref(0)
+
+</script>
+
+<template>
+  <div>
+    <h1>Event Handler</h1>
+    <h1>Count: {{ count }}</h1>
+    <!-- <button v-on:click="count++">Click</button> -->
+    <button @click="count++">Click</button>
+    <button @click="count--">Decrease</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
