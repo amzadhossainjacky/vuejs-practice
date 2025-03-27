01b8d33dbf9a391aef45c63089da49431135f037
amzadhossainjacky <amzadhossainjacky@gmail.com>
2025-03-27 14:48:17 +0600

    props completed

---
+++ ch27_props/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'; 
+
+import { ref } from 'vue'
+
+const fullName = ref('jacky')
+</script>
+
+
+<template>
+  <div>
+    <h1>Props</h1>
+    <p>
+      <!-- <HelloWorld fullName="jacky"/> -->
+
+      <!-- dynamic props -->
+      <HelloWorld :fullName="fullName"/>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
---
+++ ch27_props/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup>
+const msg = "Hello Bangladesh"
+
+const props = defineProps(['fullName'])
+</script>
+
+<template>
+    <div>
+        <h1> {{ props.fullName}} </h1>
+    </div>
+</template>
+
+<style scope>
+
+</style>
