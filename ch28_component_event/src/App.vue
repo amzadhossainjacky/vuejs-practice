f2752929ea8345f3345e80e5f4422501bbf24bcd
amzadhossainjacky <amzadhossainjacky@gmail.com>
2025-03-27 17:43:04 +0600

    compoenent event completed

---
+++ ch28_component_event/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'; 
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increaseCount() {
+  console.log('increaseCount')
+  count.value = count.value + 1
+}
+</script>
+
+<template>
+  <div>
+    <h1>Component Event</h1>
+    <h1>Count: {{ count }}</h1>
+  <!-- <HelloWorld @countByEvent="count++"/> -->
+
+  <HelloWorld @countByEvent="increaseCount"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
---
+++ ch28_component_event/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup>
+const msg = "Hello Bangladesh"
+</script>
+
+<template>
+    <div>
+        <h1> {{ msg }} </h1>
+
+    <button @click="$emit('countByEvent')">Click Me</button>
+    </div>
+</template>
+
+<style scope>
+
+</style>
