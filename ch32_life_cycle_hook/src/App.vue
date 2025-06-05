ae10a94027b9499a76db21bd0314334818a3982f
amzadhossainjacky <amzadhossainjacky@gmail.com>
2025-06-05 17:44:09 +0600

    vue js life cycle implementation

---
+++ ch32_life_cycle_hook/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'; 
+import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue'
+
+const showHelloComponent = ref(true)
+const count = ref(0)
+
+onBeforeMount(() => {
+  console.log("onBeforeMount called - Parent")
+})
+
+onMounted(() => {
+  console.log("onMounted called - Parent")
+})
+
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate called - Parent")
+})
+
+onUpdated(() => {
+  console.log("onUpdated called - Parent")
+})
+
+onBeforeUnmount(() => {
+  console.log("onBeforeUnmount called - Parent")
+})
+
+onUnmounted(() => {
+  console.log("onUnmounted called - Parent")
+})
+
+</script>
+
+<template>
+  <div>
+    <h1>Count value: {{ count }}</h1>
+    <h1>Parent component</h1>
+    <p>
+      <HelloWorld v-if="showHelloComponent"/>
+      <button @click="showHelloComponent = !showHelloComponent">Hide/Show</button>
+      <button @click="count++">Increase</button>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
---
+++ ch32_life_cycle_hook/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<script setup>
+const msg = "Hello Child Component"
+
+import { onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue'
+
+onBeforeMount(() => {
+  console.log("onBeforeMount called - child")
+})
+
+onMounted(() => {
+  console.log("onMounted called - child")
+})
+
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate called - child")
+})
+
+onUpdated(() => {
+  console.log("onUpdated called - child")
+})
+
+onBeforeUnmount(() => {
+  console.log("onBeforeUnmount called - child")
+})
+
+onUnmounted(() => {
+  console.log("onUnmounted called - child")
+})
+
+</script>
+
+<template>
+    <div>
+        <h1> {{ msg }} </h1>
+    </div>
+</template>
+
+<style scope>
+
+</style>
