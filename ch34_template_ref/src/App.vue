8c1076c025f891850bb330e1841b608576004370
amzadhossainjacky <amzadhossainjacky@gmail.com>
2025-06-06 17:02:41 +0600

    vue template ref impelementation

---
+++ ch34_template_ref/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'; 
+import { ref, onMounted, watchEffect } from 'vue'
+
+const inputRef = ref(null)
+
+/* onMounted(() => {
+  // console.log(inputRef.value)
+  inputRef.value.focus()
+})
+ */
+
+
+//Watch effected called 
+watchEffect(() => {
+  console.log("watcheffected called")
+  if (inputRef.value) {
+    
+    console.log(inputRef.value)
+    inputRef.value.focus()
+  } else {
+    console.log("not mounted yet, or the element was unmounted (e.g. by v-if)")
+  }
+})
+
+</script>
+
+<template>
+  <div>
+    <h1>Template Ref</h1>
+    <!-- <p>
+      <HelloWorld />
+    </p> -->
+
+    <input type="text" ref="inputRef" >
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
